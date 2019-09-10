749c0e7a4446aff9b46c76408786511014f8d977
ALDA <pppppaaattt@gmail.com>
2019-09-11 01:30:28 +0800

    changed p

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         TANGINA MOasdasdasdsadasd
+         hello pota
         </p>
         <a
           className="App-link"
